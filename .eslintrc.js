472310a19e051f6f122316447d2c8ee86055d5e4
manak1 <mana23happy@hotmail.co.jp>
2020-12-12 18:15:31 +0900

    chore: set up eslint/ts

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,14 +4,23 @@ module.exports = {
     browser: true,
     node: true,
   },
+  parserOptions: {
+    parser: 'babel-eslint',
+  },
   extends: [
-    '@nuxtjs/eslint-config-typescript',
-    'prettier',
-    'prettier/vue',
+    'eslint:recommended',
+    'plugin:vue/recommended',
     'plugin:prettier/recommended',
-    'plugin:nuxt/recommended',
   ],
-  plugins: ['prettier'],
-  // add your custom rules here
-  rules: {},
+  // *.vue ファイルを lint にかけるために必要
+  plugins: ['vue'],
+  // ここにカスタムルールを追加します。
+  rules: {
+    'vue/html-self-closing': 'off',
+    semi: [2, 'never'],
+    'vue/no-v-html': 'off',
+    'no-console': 'off',
+    'vue/max-attributes-per-line': 'off',
+    'prettier/prettier': ['error', { semi: false }],
+  },
 }
